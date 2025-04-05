3c5593129c509ec77ed954c1785053b8add6aa7f
Carolina Otermin <146308518+oterminA@users.noreply.github.com>
2025-04-05 17:39:44 -0300

    Update Test_Teatro.php

--- tp_1/clase_Teatro/Test_Teatro.php
+++ tp_1/clase_Teatro/Test_Teatro.php
@@ -1,21 +1,42 @@
 <?php
 include_once 'Teatro.php';
 
-echo "Nombre teatro:\n";
-$nombre = trim(fgets(STDIN));
-echo "Direccion teatro:\n";
-$direccion = trim(fgets(STDIN));
-for ($i=0; $i<2;$i++){
-    echo "Nombre: funcion " . $i+1 . "\n";
-        $funciones[$i]["nombre"]=trim(fgets(STDIN));
-        echo "Precio entrada: \n" ;
-        $funciones[$i]["precio"]=trim(fgets(STDIN));
+
+echo ">Ingrese el nombre del teatro: ";
+$nombreTeatro = trim(fgets(STDIN));
+
+echo ">Ingrese la dirección del teatro: ";
+$direccionTeatro = trim(fgets(STDIN));
+
+$funciones = [];
+for ($i = 0; $i < 4; $i++) {
+    echo ">Nombre de la función " . ($i + 1) . ": ";
+    $nombreFuncion = trim(fgets(STDIN));
+    echo ">Precio de la función: ";
+    $precioFuncion = trim(fgets(STDIN));
+    $funciones[] = [
+        "nombre" => $nombreFuncion,
+        "precio" => $precioFuncion
+    ];
 }
 
-$teatro1 = new Teatro ($nombre, $direccion, $funciones);
+$teatro1 = new Teatro($nombreTeatro, $direccionTeatro, $funciones);
 
+echo ">Ingrese un nuevo nombre para el teatro: ";
+$nuevoNombre = trim(fgets(STDIN));
+$teatro1->cambiarNombreTeatro($nuevoNombre);
+
+echo ">Ingrese una nueva dirección para el teatro: ";
+$nuevaDireccion = trim(fgets(STDIN));
+$teatro1->cambiarDireccionTeatro($nuevaDireccion);
+
+echo ">¿Qué número de función desea modificar? ";
+$indiceFuncion = (trim(fgets(STDIN)) - 1);
+echo ">Nuevo nombre de la función: ";
+$nuevoNombreFuncion = trim(fgets(STDIN));
+echo ">Nuevo precio de la función: ";
+$nuevoPrecioFuncion = trim(fgets(STDIN));
+$teatro1->cambiarFuncion($indiceFuncion, $nuevoNombreFuncion, $nuevoPrecioFuncion);
+//quiero hacer un if antes, para validar que el índice exista dentro del rango pero no puedo por los parámetros
 
 echo $teatro1;
-$teatro1->mostrarFunciones();
-
-?>
