b83493df76ba2c59387310ae3081c841533cc70d
Carolina Otermin <146308518+oterminA@users.noreply.github.com>
2025-05-30 10:21:04 -0300

    Update Vagon_Carga.php

--- simulacro_segundo_parcial/Vagon_Carga.php
+++ simulacro_segundo_parcial/Vagon_Carga.php
@@ -2,11 +2,9 @@
 /*1. Se registra la siguiente información: año de instalación, largo, ancho, peso del vagón vacío.
 Si se trata de un vagón de pasajeros también se almacena la cantidad máxima de pasajeros que puede
 transportar, la cantidad de pasajeros que está transportando y el peso promedio de los pasajeros. 
-Es importante tener en cuenta que la variable de instancia que representa el peso del vagón se calcula de acuerdo a la
-cantidad de pasajeros que se está transportando y considerando un peso promedio por pasajero de 50 Kilos..
+Es importante tener en cuenta que la variable de instancia que representa el peso del vagón se calcula de acuerdo a la cantidad de pasajeros que se está transportando y considerando un peso promedio por pasajero de 50 Kilos..
 Si se trata de un vagón de carga se almacena el peso máximo que puede transportar y el peso de su carga
-transportada. El peso del vagón va a depender del peso de su carga más un índice que coincide con un 20 % del
-peso de la carga, dicho índice se guarda en cada vagón de carga.???????
+transportada. El peso del vagón va a depender del peso de su carga más un índice que coincide con un 20 % del peso de la carga, dicho índice se guarda en cada vagón de carga.
 2. Implementar el método incorporarPasajeroVagon que recibe por parámetro la cantidad de pasajeros
 que ingresan al vagón y tiene la responsabilidad de actualizar las variables instancias que representan el
 peso y la cantidad actual de pasajeros.El método debe devolver verdadero o falso según si se pudo o no
@@ -22,17 +20,17 @@ class Vagon_Carga extends Vagon
     //atributos (variables instancia)
     private $pesoMaximoTransporta;
     private $pesoCargaTransportada;
-    //donde va el índice=0.2????
-    private $pesoTotal;
+    private $indice;
 
     //metodo constructor
-    public function __construct($anio, $largo, $ancho, $pesoVacio, $pesoTransporte, $pesoCarga, $total)
+    public function __construct($anio, $largo, $ancho, $pesoVacio, $pesoTransporte)
     {
         parent::__construct($anio, $largo, $ancho, $pesoVacio);
         //pongo el atributo normalmente
         $this->pesoMaximoTransporta = $pesoTransporte;
-        $this->pesoCargaTransportada = $pesoCarga;
-        $this->pesoTotal = $total;
+        $this->pesoCargaTransportada = 0;
+        $this->indice=0.2; ///ya puesto como decimal
+
     }
 
     //metodos de accedo: getters  y setters
@@ -45,6 +43,11 @@ class Vagon_Carga extends Vagon
     {
         return $this->pesoCargaTransportada;
     }
+    public function getIndice()
+    {
+        return $this->indice;
+    }
+
 
     //set
     public function setPesoMaximoTransporta($pesoTransporte)
@@ -55,6 +58,11 @@ class Vagon_Carga extends Vagon
     {
         $this->pesoCargaTransportada = $pesoCarga;
     }
+    public function setIndice($indice)
+    {
+        $this->indice = $indice;
+    }
+
 
     //redefinicion metodo __toString()
     public function __toString()
@@ -62,22 +70,25 @@ class Vagon_Carga extends Vagon
         $mensaje =
             parent::__toString() .
             "Peso maximo que se transporta: " . $this->getPesoMaximoTransporta() . "kg\n" .
-            "Peso carga transportada: " . $this->getPesoCargaTransportada() . "kg\n";
+            "Peso carga transportada: " . $this->getPesoCargaTransportada() . "kg\n".
+            "Indice: " . $this->getIndice(). "\n" ;
         return $mensaje;
     }
 
     //metodos nuevos
     /**
-    * metodo polimorfico, redefinicion de calcularPesoVagon()
+    * redefinicion de calcularPesoVagon()
+    * El peso del vagón va a depender del peso de su carga más un índice que coincide con un 20 % del
+peso de la carga, dicho índice se guarda en cada vagón de carga.
     */
-    // public function calcularPesoVagon(){
-    //     $pesoPadre = parent::calcularPesoVagon(); //acá se guarda el resultado del peso del vagon vacio, metodo heredado
-    //     $pesoCarga = $this->getPesoCargaTransportada();//guardo el peso de la carga
-    //     $pesoCargaActualizado = $pesoPadre + ($pesoCarga + ($pesoCarga * 0.2)); //El peso del vagón va a depender del peso de su carga más un índice que coincide con un 20 % del peso de la carga, dicho índice se guarda en cada vagón de carga?
-    //     $this->setPesoVagonVacio($pesoCargaActualizado);
-        // return $pesoCargaActualizado; //devuelve el peso actualizado del vaogon    
-    }//
-    
+    public function calcularPesoVagon(){
+        $pesoPadre = parent::calcularPesoVagon(); //acá se guarda el resultado del peso del vagon vacio, metodo heredado
+        $pesoCarga = $this->getPesoCargaTransportada();//guardo el peso de la carga
+        $indice= $this->getIndice();
+        $pesoCargaActualizado = $pesoPadre + ($pesoCarga + ($pesoCarga * $indice)); 
+        $this->setPesoVagonVacio($pesoCargaActualizado);
+        return $pesoCargaActualizado; //devuelve el peso actualizado del vaogon    
+    }
 
 
     /**
@@ -91,9 +102,8 @@ class Vagon_Carga extends Vagon
         $pesoMaximoTransporta = $this-> getPesoMaximoTransporta(); //guardo el peso max que se puede transportar
         $sumaPesos = $pesoCarga + $carga; //sumo los pesos de las cargas para comparar con el peso maximo que se transporta
         if ($sumaPesos <= $pesoMaximoTransporta) { //si la suma de ambos no excede el peso maximo que se puede transportar, hago:
-            $pesoPadre = parent::calcularPesoVagon(); //acá se guarda el resultado del peso del vagon vacio, metodo heredado
-            $pesoCargaActualizado = $pesoPadre + ($carga + ($carga * 0.2)); //El peso del vagón va a depender del peso de su carga más un índice que coincide con un 20 % del peso de la carga, dicho índice se guarda en cada vagón de carga?
-            $this->setPesoVagonVacio($pesoCargaActualizado); //actualizo el peso del vagon
+            $this->setPesoCargaTransportada($sumaPesos); //actualizo el peso de la carga de la variable del padre
+            $calculoPeso = $this->calcularPesoVagon(); //llamo a ese metodo
             $agregado = true; //se pudo agregar a los pasajeros
         }
         return $agregado; //devuelve boolean, true si se agregó
