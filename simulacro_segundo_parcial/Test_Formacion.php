c3ae59298a6ac016b7ca0fbe2032044daf2192f3
Carolina Otermin <146308518+oterminA@users.noreply.github.com>
2025-05-30 10:22:42 -0300

    Update Test_Formacion.php

--- simulacro_segundo_parcial/Test_Formacion.php
+++ simulacro_segundo_parcial/Test_Formacion.php
@@ -10,13 +10,16 @@ $objLocomotora = new Locomotora(188000, 140);
 
 // 2. Se crea 3 objetos con la información visualizada en la tabla:
 // Vagon de pasajeros 1: peso vacío 15000, máx 30 pasajeros, 25 actuales
-$objVagon1 = new Vagon_Pasajeros(2013, 20, 3, 15000, 30, 25);
+$objVagon1 = new Vagon_Pasajeros(2013, 20, 3, 15000, 30);
+$objVagon1->setCantidadPasajerosTransporta(25); //acá seteo la cantidad de gente actual en el vagon, porque ese parametro no entra en el constructor
 
 // Vagon de carga: peso vacío 15000, carga actual 55000
 $objVagonCarga = new Vagon_Carga(2000, 20, 3, 15000, 60000, 55000);
 
 // Vagon de pasajeros 2: peso vacío 15000, máx 50 pasajeros, 50 actuales
-$objVagon2 = new Vagon_Pasajeros(2024, 20, 3, 15000, 50, 50);
+$objVagon2 = new Vagon_Pasajeros(2024, 20, 3, 15000, 50);
+$objVagon2->setCantidadPasajerosTransporta(50);//acá seteo la cantidad de gente actual en el vagon, porque ese parametro no entra en el constructor
+
 
 // 3. Se crea un objeto formación que tiene la locomotora y los vagones creados en (1) y (2) usando el método incorporarVagonFormacion
 $objFormacion = new Formacion($objLocomotora, [$objVagon1, $objVagonCarga, $objVagon2], 5); ///???
@@ -26,7 +29,7 @@ $objFormacion->incorporarVagonFormacion($objVagon2);
 
 // 4. Invocar al método incorporarPasajeroFormacion con el parámetros cantidad de pasajero = 6 y visualizar el resultado
 echo "Incorporacion de 6 pasajeros: \n";
-$resultado = $objFormacion->incorporarPasajeroFormacion(90);
+$resultado = $objFormacion->incorporarPasajeroFormacion(6);
 echo $resultado ? "Sucedió la incorporación. \n" : "Falló la incorporación. \n"; //uso una ternaria en lugar del if
 
 // 5. Realizar un print de los 3 objetos vagones creados
@@ -35,8 +38,8 @@ echo "Vagón de carga----\n " . $objVagonCarga . "\n";
 echo "Vagón de pasajeros 2----\n " . $objVagon2 . "\n";
 
 // 6. Invocar al método incorporarPasajeroFormacion con el parámetros cantidad de pasajero = 14 y visualizar el resultado
-echo "Incorporacion de 14 pasajeros: \n";
-$resultado = $objFormacion->incorporarPasajeroFormacion(200);
+echo "Incorporacion de pasajeros: \n";
+$resultado = $objFormacion->incorporarPasajeroFormacion(14);
 echo $resultado ? "Sucedió la incorporación. \n" : "Falló la incorporación. \n";
 
 // 7. Realizar un print del objeto locomotora
